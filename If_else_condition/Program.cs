db1d8ee97dd00e54f3eef0edc81f6d01fc33e8c9
adctd <yoshimoto5@rambler.ru>
2022-09-19 12:01:14 +0300

    Добавила папку if_else

---
+++ If_else_condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "adctd")
+{
+    Console.WriteLine("Yeaaaaaah, hey hey hey!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
