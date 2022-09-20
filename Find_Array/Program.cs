c5b0b01ef8c9f17c37c565cf96dab08eb2180e55
adctd <yoshimoto5@rambler.ru>
2022-09-21 01:13:37 +0300

    добавила задачи из второй лекции

---
+++ Find_Array/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 23, 34, 75, 53, 14, 17, 39, 75};
+
+int n = array.Length;
+int find = 75;
+
+int index = 0;
+
+while(index<n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
